d659e0ed23ea753c78c5a323781d5ce2d44eac1a
Anna Veselova <anna.veselova.97@mail.ru>
2026-05-26 00:26:16 +0300

    update map_player_data

--- app/mappers/player.py
+++ app/mappers/player.py
@@ -29,11 +29,8 @@ def map_player_data(
     summoner: RiotSummonerSchema,
     is_tracked: bool = True,
 ) -> dict[str, Any]:
-    return {
-        'puuid': account.puuid,
-        'game_name': account.game_name,
-        'tag_line': account.tag_line,
-        'summoner_level': summoner.summoner_level,
-        'profile_icon_id': summoner.profile_icon_id,
-        'is_tracked': is_tracked,
-    }
+    player = account.model_dump()
+    player.update(summoner.model_dump())
+    player['is_tracked'] = is_tracked
+    
+    return player
