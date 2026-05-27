ec9dd792a9782edc350e8f47836aa0ac45b5be18
Anna Veselova <anna.veselova.97@mail.ru>
2026-05-27 12:04:10 +0300

    add find_player_by_riot_id to PlayerService

--- app/services/player.py
+++ app/services/player.py
@@ -4,10 +4,41 @@ from app.constants import QueueId
 from app.core import UnitOfWork
 from app.services.dto import ChampionStatsDTO, MatchSummaryDTO, PlayerProfileDTO
 
+from .sync import SyncService
+
 
 class PlayerService:
-    def __init__(self, *, uow_factory: Callable[[], UnitOfWork]) -> None:
+    def __init__(
+            self,
+            *,
+            uow_factory: Callable[[], UnitOfWork],
+            sync_service: SyncService,
+    ) -> None:
         self._uow_factory = uow_factory
+        self._sync_service = sync_service
+
+    async def find_player_by_riot_id(
+            self,
+            *,
+            platform: str,
+            game_name: str,
+            tag_line: str,
+    ) -> str:
+        async with self._uow_factory() as uow:
+            player = await uow.players.get_profile_by_riot_id(
+                game_name=game_name,
+                tag_line=tag_line,
+            )
+
+        if player is not None:
+            return player.puuid
+
+        puuid = await self._sync_service.sync_player_profile(
+            platform=platform,
+            game_name=game_name,
+            tag_line=tag_line,
+        )
+        return puuid
 
     async def get_profile(self, puuid: str) -> PlayerProfileDTO | None:
         async with self._uow_factory() as uow:
