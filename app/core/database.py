9a7d2286cae19d3740ee99c71fed3b2267cd443a
Anna Veselova <anna.veselova.97@mail.ru>
2026-05-27 23:47:30 +0300

    add pool_pre_ping

--- app/core/database.py
+++ app/core/database.py
@@ -11,6 +11,7 @@ from app.core.config import settings
 engine = create_async_engine(
     settings.async_database_url,
     echo=False,
+    pool_pre_ping=True,
 )
 
 async_session_factory = async_sessionmaker(
